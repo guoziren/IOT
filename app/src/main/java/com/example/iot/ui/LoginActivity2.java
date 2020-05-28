7e21b99569a305b23617d4a1048006c700b02894
guoziren <234917515@qq.com>
2020-05-28 15:23:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/iot/ui/LoginActivity2.java
@@ -0,0 +1,16 @@
+package com.example.iot.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.iot.R;
+
+public class LoginActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
